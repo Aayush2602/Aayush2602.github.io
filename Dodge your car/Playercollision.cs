bf59e7dccc1d447275123e5643cb5935621a72fe
Aayush2602 <60109614+Aayush2602@users.noreply.github.com>
2021-09-28 14:07:11 -0400

    Create Playercollision.cs

---
+++ Dodge your car/Playercollision.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class Playercollision : MonoBehaviour
+{
+    public Playermovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<Gamemanager>().EndGame();
+        }
+    }
+}
